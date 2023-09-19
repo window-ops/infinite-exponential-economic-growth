a6521ab7a4fa30f405b2e21b88e69c1991fcca8a
window-ops <vladbruma2020@gmail.com>
2023-09-19 17:22:47 +0000

    Update file plot.py

--- plot.py
+++ plot.py
@@ -11,8 +11,8 @@ def exponential_growth(x, a, b):
 a = 10  # economy
 b = 0.3  # growth factor
 
-# Define the maximum sustainable economic growth
-max_growth = 3000
+# Define the overshoot point
+overshoot = 3000
 
 
 # Define a function to update the plot when the view changes
@@ -31,7 +31,7 @@ y = exponential_growth(x, a, b)
 line, = ax.plot(x, y, linewidth=2, label='Economic Growth')
 
 # Add a horizontal line to indicate the overshoot line
-ax.axhline(y=max_growth, color='r', linestyle='--', label='Overshoot Line')
+ax.axhline(y=overshoot, color='r', linestyle='--', label='Overshoot Line')
 
 # Set the x and y-axis labels
 ax.set_xlabel('Time')
